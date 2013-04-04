07b41a777d5f603dce7e33760cc99270764fa3ad
Harley Inbody <hinbody@gmail.com>
2013-04-04 18:26:35 -0400

    Generate Playtime Jumpers as a Rails 4 application

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+PlaytimeJumpers::Application.config.secret_key_base = '8b1478df6f33afc83fb75d3cc68568031398512a88dab0a24940534e5fc9a53de4769b65184c117bd6364d17ad02f6e682a7ce4cb4a963041c018bc8995e498e'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+PlaytimeJumpers::Application.config.session_store :encrypted_cookie_store, key: '_playtime_jumpers_session'
